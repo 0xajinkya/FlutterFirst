0a9aabaf94ddbb845a8fbe93f90effa6cd18a3dc
Ajinkya <kahaneajinkya051@gmail.com>
2022-11-29 16:59:12 +0530

    Basic Fluttering

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
 
+int _count = 0;
+
 void main() {
-  runApp(const MaterialApp(
-    home: Text('Heylo'),
+
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("It's My First Time Doing This"),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+      ),
+      body: const Center(
+        child: Text(
+            'Hello Mommy',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              backgroundColor: Colors.purple,
+              color: Colors.white,
+              letterSpacing: 2.0,
+              fontFamily: 'Nunito'
+            ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Text('Click'),
+        onPressed: () {},
+        backgroundColor: Colors.purple[800],
+      ),
+    ),
   ));
 }
+
+
